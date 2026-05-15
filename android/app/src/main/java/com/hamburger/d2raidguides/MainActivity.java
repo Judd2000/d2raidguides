470c599e1c7f8438e275b9ff5dbfe9bcef755ae5
Judd2000 <njudd.mail@gmail.com>
2026-05-15 08:24:58 -0700

    Add IOS and Android files to gitignore

--- android/app/src/main/java/com/hamburger/d2raidguides/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.hamburger.d2raidguides;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.hamburger.d2raidguides',
+  appId: 'com.j2k.d2raidguides',
   appName: 'd2raidguides',
   webDir: 'dist/d2raidguides/browser',
   server: {
