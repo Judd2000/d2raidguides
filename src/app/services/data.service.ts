455744f0f2d4351965cc5422835291eae04ffd13
Judd2000 <njudd.mail@gmail.com>
2026-08-14 19:25:19 -0700

    Base typing changes and vulnerability fixes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,9 +13,7 @@ export class DataService {
       const url = `${serverUrl}/getdata/manifestdata`;
 
       CapacitorHttp.get({ url }).then((r) => {
-        // TODO: ADD ERROR TOASTS
         if (r?.status === 200 && r?.data) {
-          // add to local storage.
           localStorage.setItem('manifestData', JSON.stringify(r.data));
         } else {
           // TODO: Better error handling in-app (toasts?)
